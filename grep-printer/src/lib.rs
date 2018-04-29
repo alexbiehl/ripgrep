efd5db158af268b05ac1a7dc8c1129a03a5d49f4
Andrew Gallant <jamslam@gmail.com>
2018-04-29 09:29:52 -0400

    libripgrep: initial commit introducing libripgrep
    libripgrep is not any one library, but rather, a collection of libraries
    that roughly separate the following key distinct phases in a grep
    implementation:
    
      1. Pattern matching (e.g., by a regex engine).
      2. Searching a file using a pattern matcher.
      3. Printing results.
    
    Ultimately, both (1) and (3) are defined by de-coupled interfaces, of
    which there may be multiple implementations. Namely, (1) is satisfied by
    the `Matcher` trait in the `grep-matcher` crate and (3) is satisfied by
    the `Sink` trait in the `grep2` crate. The searcher (2) ties everything
    together and finds results using a matcher and reports those results
    using a `Sink` implementation.

---
+++ grep-printer/src/lib.rs
@@ -0,0 +1,30 @@
+#![allow(dead_code, unused_imports)]
+
+extern crate grep2;
+extern crate grep_matcher;
+#[cfg(test)]
+extern crate grep_regex;
+#[macro_use]
+extern crate log;
+#[cfg(feature = "serde1")]
+extern crate serde;
+#[cfg(feature = "serde1")]
+#[macro_use]
+extern crate serde_derive;
+extern crate termcolor;
+
+pub use color::UserColorSpec;
+pub use standard::{Standard, StandardBuilder, StandardSink};
+pub use stats::Stats;
+
+#[macro_use]
+mod macros;
+
+mod ackmate;
+mod color;
+mod counter;
+#[cfg(feature = "serde1")]
+mod json;
+mod standard;
+mod stats;
+mod util;
---
+++ grep2/src/lib.rs
@@ -0,0 +1,35 @@
+/*!
+grep, as a library.
+*/
+
+#![deny(missing_docs)]
+
+extern crate bytecount;
+extern crate grep_matcher;
+extern crate memchr;
+#[cfg(test)]
+extern crate regex;
+
+pub use lines::{LineIter, LineStep};
+pub use grep_matcher::Matcher;
+pub use searcher::{
+    BinaryDetection, ConfigError, MmapChoice,
+    Searcher, SearcherBuilder,
+};
+pub use sink::{
+    Sink, SinkError,
+    SinkContext, SinkContextKind, SinkFinish, SinkMatch,
+};
+pub use sink::sinks;
+
+/// Traits and types for the underlying matcher implementation.
+pub mod matcher {
+    pub use grep_matcher::*;
+}
+
+mod line_buffer;
+mod lines;
+mod searcher;
+mod sink;
+#[cfg(test)]
+mod testutil;
